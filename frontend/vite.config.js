e6284d163ed39d08546742d83f56b9e4ad2cad03
Mona Lisa <random>
2025-06-13 21:55:57 +0300

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,13 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,                 // слушаем на всех интерфейсах
+    host: true, // слушаем на всех интерфейсах
     port: 3000,
-    allowedHosts: [
-      'localhost',
-      'dofrag.com'               // разрешаем запросы с dofrag.com
-    ]
+    allowedHosts: 'all',
   },
   // остальная конфигурация...
-})
+});
