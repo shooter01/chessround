7b64a992c5d509bd4b9f03c31574828b8f913ec2
Mona Lisa <random>
2025-06-13 19:56:53 +0300

    prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Позволяет подключаться извне контейнера
-    port: 3000, // Порт Vite
-    watch: {
-      usePolling: true, // Включаем polling для Docker
-    },
+    host: true,                 // слушаем на всех интерфейсах
+    port: 3000,
+    allowedHosts: [
+      'localhost',
+      'dofrag.com'               // разрешаем запросы с dofrag.com
+    ]
   },
-});
+  // остальная конфигурация...
+})
